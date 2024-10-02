8d2d65d00d9ef1a1dc9ff460f433ef1c3287871a
Paulo Victor <victorlage7@gmail.com>
2024-10-01 21:47:33 -0300

    testes

--- WebApiTest/Controllers/ContactsControllerTest.cs
+++ WebApiTest/Controllers/ContactsControllerTest.cs
@@ -41,25 +41,6 @@ public class ContactsControllerTest
     }
 
     [Test]
-    public async Task CreateContact_ShouldReturnOk()
-    {
-        // Arrange
-        _contactAppServiceMock.Setup(x => x.AddAsync(_contactViewModel))
-            .ReturnsAsync(new ResultValidation
-            {
-                Object = _contactViewModel
-            });
-
-        // Act
-        var result = await _controllerMock.AddContactAsync(_contactViewModel);
-        var okResult = result as OkObjectResult;
-
-        // Assert
-        Assert.That(okResult, Is.Not.Null);
-        Assert.That(okResult.StatusCode, Is.EqualTo(200));
-    }
-
-    [Test]
     public async Task GetContacts_ShouldReturnOk()
     {
         // Arrange
@@ -80,60 +61,7 @@ public class ContactsControllerTest
         Assert.That(okResult.StatusCode, Is.EqualTo(200));
     }
 
-    [Test]
-    public async Task GetByContactId_ShouldReturnOk()
-    {
-        // Arrange
-        var contactId = new Guid();
-
-        _contactAppServiceMock.Setup(x => x.GetByContactIdAsync(contactId))
-            .ReturnsAsync(_contactViewModel);
-
-        // Act
-        var result = await _controllerMock.GetByContactIdAsync(contactId);
-        var okResult = result as OkObjectResult;
-
-        // Assert
-        Assert.That(okResult, Is.Not.Null);
-        Assert.That(okResult.StatusCode, Is.EqualTo(200));
-    }
-
-    [Test]
-    public async Task UpdateContact_ShouldReturnOk()
-    {
-        // Arrange
-        _contactAppServiceMock.Setup(x => x.UpdateAsync(_contactViewModel))
-            .ReturnsAsync(new ResultValidation
-            {
-                Object = _contactViewModel
-            });
-
-        // Act
-        var result = await _controllerMock.UpdateContactAsync(_contactViewModel);
-        var okResult = result as OkObjectResult;
-
-        // Assert
-        Assert.That(okResult, Is.Not.Null);
-        Assert.That(okResult.StatusCode, Is.EqualTo(200));
-    }
-
-    [Test]
-    public async Task DeleteContact_ShouldReturnOk()
-    {
-        // Arrange
-        var contactId = new Guid();
-
-        _contactAppServiceMock.Setup(x => x.DeleteAsync(contactId))
-            .ReturnsAsync(true);
-
-        // Act
-        var result = await _controllerMock.DeleteContactAsync(contactId);
-        var okResult = result as OkObjectResult;
-
-        // Assert
-        Assert.That(okResult, Is.Not.Null);
-        Assert.That(okResult.StatusCode, Is.EqualTo(200));
-    }
+   
 
     [Test]
     public async Task CreateContact_ShouldReturnNotOk()
@@ -176,59 +104,5 @@ public class ContactsControllerTest
         Assert.That(notFoundResult.StatusCode, Is.EqualTo(404));
     }
 
-    [Test]
-    public async Task GetByContactId_ShouldReturnNotOk()
-    {
-        // Arrange
-        var contactId = new Guid();
-
-        _contactAppServiceMock.Setup(x => x.GetByContactIdAsync(contactId))
-            .ReturnsAsync(() => null);
-
-        // Act
-        var result = await _controllerMock.GetByContactIdAsync(contactId);
-        var notFoundResult = result as NotFoundObjectResult;
-
-        // Assert
-        Assert.That(notFoundResult, Is.Not.Null);
-        Assert.That(notFoundResult.StatusCode, Is.EqualTo(404));
-    }
-
-    [Test]
-    public async Task UpdateContact_ShouldReturnNotOk()
-    {
-        // Arrange
-
-        _contactAppServiceMock.Setup(x => x.UpdateAsync(_invalidContactViewModel))
-            .ReturnsAsync(new ResultValidation
-            {
-                ValidationResults = [new ValidationResult("error")]
-            });
-
-        // Act
-        var result = await _controllerMock.UpdateContactAsync(_invalidContactViewModel);
-        var badRequestResult = result as BadRequestObjectResult;
-
-        // Assert
-        Assert.That(badRequestResult, Is.Not.Null);
-        Assert.That(badRequestResult.StatusCode, Is.EqualTo(400));
-    }
-
-    [Test]
-    public async Task DeleteContact_ShouldReturnNotOk()
-    {
-        // Arrange
-        var contactId = new Guid();
-
-        _contactAppServiceMock.Setup(x => x.DeleteAsync(contactId))
-            .ReturnsAsync(false);
-
-        // Act
-        var result = await _controllerMock.DeleteContactAsync(contactId);
-        var notFoundResult = result as NotFoundObjectResult;
-
-        // Assert
-        Assert.That(notFoundResult, Is.Not.Null);
-        Assert.That(notFoundResult.StatusCode, Is.EqualTo(404));
-    }
+  
 }
